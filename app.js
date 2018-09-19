a8106406cb008aa21e6667ed540d2c25353422e4
Benjamin Austin <bnjmn80@gmail.com>
2018-09-19 15:01:05 -0600

    Updated app.js with server set-up consts and app.listen function.

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const knex = require('knex');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const port = process.env.PORT || 9000
 const someday = require('./routes/someday.js');
 const yesterday = require('./routes/yesterday.js');
 
 app.use(cors());
 app.use(bodyParser.json());
 
+app.listen(port, () => {
+    console.log(`I am listening on ${port}`)
+})
